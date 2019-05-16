cb9fc35ece7bf7442e19fbbae32b31e93250b302
Antonio Silva <antoni-s@outlook.com>
2019-05-16 13:26:26 -0300

    Update JadeContainer.java

--- src/ma/ensias/sma/JadeContainer.java
+++ src/ma/ensias/sma/JadeContainer.java
@@ -8,14 +8,14 @@ public class JadeContainer {
 	private ContainerController container;
 	
 	public JadeContainer() {
-		// Créer une instance de la MV
+		// Crie uma instÃ¢ncia da VM
 		Runtime rt = Runtime.instance();
-		// Pas de propriétés, ce n'et pas un main container, mais un profile !
+		// Criar perfil sem propriedade
 		ProfileImpl profile = new ProfileImpl(false);
-		// Le main container associé est déjà démarré sur localhost
+		// IniciaÃ§Ã£o do perfil
 		profile.setParameter(ProfileImpl.MAIN_HOST, "localhost");
 		profile.setParameter(ProfileImpl.CONTAINER_NAME, "Market");
-		container = rt.createAgentContainer(profile); // Créer le container
+		container = rt.createAgentContainer(profile); // criar os container
 	}
 
 	public ContainerController getContainer() { return container; }
